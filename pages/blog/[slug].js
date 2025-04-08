709817309b4a9b0324d69cc98d3f38fb830d5208
Felipe Develop <camachorodriguezandresfelipe@gmail.com>
2025-04-08 14:49:38 -0500

    Agrega blog local sin backend usando datos simulados

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,28 +1,20 @@
-// import { getAllBlogSlugs, getBlogPostBySlug } from '../../lib/strapi';
+// pages/blog/index.js
+import Link from 'next/link';
+import { getAllBlogSlugs } from '../../data/posts';
 
-// export async function getStaticPaths() {
-//   const slugs = await getAllBlogSlugs(); // ['post-1', 'post-2', ...]
-  
-//   const paths = slugs.map((slug) => ({
-//     params: { slug },
-//   }));
+export default function BlogIndex() {
+  const slugs = getAllBlogSlugs();
 
-//   return {
-//     paths,
-//     fallback: false, // Puedes cambiar a true o 'blocking' si lo necesitas
-//   };
-// }
-
-// export async function getStaticProps({ params }) {
-//   const post = await getBlogPostBySlug(params.slug);
-//   return { props: { post } };
-// }
-
-// export default function PostPage({ post }) {
-//   return (
-//     <article>
-//       <h1>{post.title}</h1>
-//       <div dangerouslySetInnerHTML={{ __html: post.content }} />
-//     </article>
-//   );
-// }
+  return (
+    <div>
+      <h1>Blog</h1>
+      <ul>
+        {slugs.map((slug) => (
+          <li key={slug}>
+            <Link href={`/blog/${slug}`}>{slug}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
