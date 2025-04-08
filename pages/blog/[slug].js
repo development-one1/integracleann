62f531c19d6c9a49a2d1764e6a484c45ed54685c
Felipe Develop <camachorodriguezandresfelipe@gmail.com>
2025-04-08 14:39:19 -0500

    Corrige getStaticPaths en página de blog

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,28 +1,28 @@
-import { getAllBlogSlugs, getBlogPostBySlug } from '../../lib/strapi';
+// import { getAllBlogSlugs, getBlogPostBySlug } from '../../lib/strapi';
 
-export async function getStaticPaths() {
-  const slugs = await getAllBlogSlugs(); // ['post-1', 'post-2', ...]
+// export async function getStaticPaths() {
+//   const slugs = await getAllBlogSlugs(); // ['post-1', 'post-2', ...]
   
-  const paths = slugs.map((slug) => ({
-    params: { slug },
-  }));
+//   const paths = slugs.map((slug) => ({
+//     params: { slug },
+//   }));
 
-  return {
-    paths,
-    fallback: false, // Puedes cambiar a true o 'blocking' si lo necesitas
-  };
-}
+//   return {
+//     paths,
+//     fallback: false, // Puedes cambiar a true o 'blocking' si lo necesitas
+//   };
+// }
 
-export async function getStaticProps({ params }) {
-  const post = await getBlogPostBySlug(params.slug);
-  return { props: { post } };
-}
+// export async function getStaticProps({ params }) {
+//   const post = await getBlogPostBySlug(params.slug);
+//   return { props: { post } };
+// }
 
-export default function PostPage({ post }) {
-  return (
-    <article>
-      <h1>{post.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.content }} />
-    </article>
-  );
-}
+// export default function PostPage({ post }) {
+//   return (
+//     <article>
+//       <h1>{post.title}</h1>
+//       <div dangerouslySetInnerHTML={{ __html: post.content }} />
+//     </article>
+//   );
+// }
